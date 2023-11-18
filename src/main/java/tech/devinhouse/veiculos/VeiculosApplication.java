2834e87c5fe1d345e063070d606fe6d7329bafeb
unknown <neto.acpn@gmail.com>
2023-11-18 11:30:41 -0300

    Adiciona Exercicio07

--- src/main/java/tech/devinhouse/veiculos/VeiculosApplication.java
+++ src/main/java/tech/devinhouse/veiculos/VeiculosApplication.java
@@ -35,6 +35,9 @@ public class VeiculosApplication {
 			multaRepo.save(multa1);
 			multaRepo.save(multa2);
 			multaRepo.save(multa3);
+
+			multa3.setValor(380F);
+			multaRepo.save(multa3);
 		};
 	}
 }
